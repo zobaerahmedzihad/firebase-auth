260cb581a1ba9e57af8ce2e9241b8dbede3960cc
zobaerahmedzihad <zobaerahmedzihad02@gmail.com>
2021-03-17 03:28:42 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
 import './App.css';
+import firebaseConfig from './firebase.config'
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+// firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+   const [user , setUser] = useState({
+     isSignedIn: false,
+     name: '', 
+     email: '',
+     password: '',
+     photo : ''
+   })
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const handleSineIn = () => {
+      firebase.auth().signInWithPopup(provider)
+     
+        .then( res => {
+          const {displayName , email , photoURL} = res.user;
+          const signedInUser = {
+            isSignedIn : true,
+            name : displayName,
+            email : email,
+
+            photo : photoURL
+          }
+          setUser(signedInUser);
+          // console.log(displayName , email , photoURL);                                         
+        })
+      .catch(error => {
+        console.log(error);
+        console.log(error.message);
+      }) 
+    }
+    const {displayName , email , photo} = user;
+           
+    const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then((res)=> {
+        const userSignOut = {
+          isSignedIn : false,
+          name: '',
+          email: '',
+          photo: ''
+        }
+    setUser(userSignOut)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+
+
+
+    const handleBlur = (e) => {
+      let isFieldValid = true;
+        console.log(e.target.name , e.target.value);
+        if(e.target.name === 'email'){
+          isFieldValid = /\S+@\S+\.\S+/.test(e.target.value)
+          
+        }
+        if(e.target.name === 'password'){
+            const isPasswordValid = e.target.value.length > 6
+            const passwordHasNumber =  /\d{1}/.test(e.target.value)
+            isFieldValid = passwordHasNumber && isPasswordValid
+         
+        }
+        if(isFieldValid){
+          const newUserInfo = {...user}
+          newUserInfo[e.target.name] = e.target.value;
+          setUser(newUserInfo)
+        }
+    }
+
+    const handleSubmit = () => {
+
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'style1' >
+      
+      {
+        user.isSignedIn ?    <button onClick = {handleSignOut} >Sign out</button> :
+        <button onClick = {handleSineIn} >Sing In</button>
+      }
+    
+     {
+       user.isSignedIn &&
+        <div> 
+         
+          <h2>Welcome to , {displayName} </h2>
+          <h3>Email : {email} </h3>
+          <img src={photo} alt=""/>
+        </div>
+     }
+                                 {/* module 41  */}
+
+     <div className = "style1">
+      <h1> Our Own Authentication System. </h1>
+      <form onSubmit = {handleSubmit}>
+          <input type="text" name="name" onBlur = {handleBlur} placeholder = "Name"/><br/><br/>
+          <input type="email" onBlur = {handleBlur} name = "email" placeholder ="Enter Your Email" required /><br/><br/>
+          <input type="password" onBlur = {handleBlur} name = "password" placeholder ="Enter Your Password" required /><br/><br/>
+          <input type="button" value="Submit"/>
+
+      </form>
+     </div>
+
     </div>
   );
 }
 
 export default App;
+
+ // const provider = new firebase.auth.GoogleAuthProvider();
+  // const handleSineIn =()=> {
+  //   firebase.auth().signInWithPopup(provider)
+  //     .then(res => {
+  //       const {displayName , photoURL , email} = res.user;
+  //       console.log(displayName , photoURL , email);
+  //     })
+  // }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARElnM1rgDoxGtkbz8aJk3wTEKYVxWN_0",
+    authDomain: "ema-john-simple02.firebaseapp.com",
+    projectId: "ema-john-simple02",
+    storageBucket: "ema-john-simple02.appspot.com",
+    messagingSenderId: "1082879630740",
+    appId: "1:1082879630740:web:706e155b253ce37d934381"
+  };
+
+  export default firebaseConfig;
